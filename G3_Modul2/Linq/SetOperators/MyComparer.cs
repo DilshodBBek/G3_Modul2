0cadf6baf0f4cc206beb3f1834567667439d93ed
Dilshod <dilshodsh97@gmail.com>
2023-07-19 18:04:22 +0500

    sdfsd

--- G3_Modul2/Linq/SetOperators/MyComparer.cs
+++ G3_Modul2/Linq/SetOperators/MyComparer.cs
@@ -1,27 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics.CodeAnalysis;
 
-namespace G3_Modul2.Linq.SetOperators
+namespace G3_Modul2.Linq.SetOperators;
+
+public class MyComparer : IEqualityComparer<string>, IComparer<string>
 {
-    public class MyComparer : IEqualityComparer<string>, IComparer<string>
+    public int Compare(string? x, string? y)
     {
-        public int Compare(string? x, string? y)
-        {
-            return x.Equals(y, StringComparison.OrdinalIgnoreCase) ? 0 : 1;
-        }
+        return x.Equals(y, StringComparison.OrdinalIgnoreCase) ? 0 : 1;
+    }
 
-        public bool Equals(string? x, string? y)
-        {
-            return x.ToLower() == y.ToLower();// x.Equals(y, StringComparison.OrdinalIgnoreCase);
-        }
+    public bool Equals(string? x, string? y)
+    {
+        return x.ToLower() == y.ToLower();// x.Equals(y, StringComparison.OrdinalIgnoreCase);
+    }
 
-        public int GetHashCode([DisallowNull] string obj)
-        {
-            return obj.GetHashCode();
-        }
+    public int GetHashCode([DisallowNull] string obj)
+    {
+        return obj.GetHashCode();
     }
 }
